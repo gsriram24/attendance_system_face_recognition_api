e4b570ae0d76f013e2143bc4854802f2a804d310
sgcuber24 <sgcuber24@gmail.com>
2021-04-11 17:20:34 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Attendance System Face Recognition API
+
+A sample Flask application which helps with the face recognition in the [Attendance System App](https://github.com/sgcuber24/attendance_system_app)  
+
+
+## Getting Started
+
+To get started, you'll want to first clone this GitHub repository locally:
+
+```bash
+$ git clone https://github.com/sgcuber24/attendance_system_face_recognition_api.git
+```
+
+Next, you'll want to go into the sample app directory:
+
+```bash
+$ cd attendance_system_face_recognition_api
+```
+
+Then you'll want to install all of the Python requirements (via
+[pip](http://pip.readthedocs.org/en/latest/)):
+
+```bash
+$ pip install -r requirements.txt
+```
+
+And lastly, you'll want to run the `app.py` script which will guide you
+the rest of the way:
+
+```bash
+$ python app.py
+```
+
+**Note**: Since this API is built for the [Attendance System App](https://github.com/sgcuber24/attendance_system_app), you will have to be authorized as a Teacher and must pass in a JWT (JSON Web Token) with your request. You can find the Attendance System Backend [here](https://github.com/Josh551/attendance-system-backend)
+
+
+## Available end-points
+
+
+### POST /
+
+Takes in a File and class ID and returns the students present in the class after performing facial recognition and detection. 
+
+**Headers**
+
+Content-Type : form-data
+
+**Request body**
+
+```
+| Key | Value |
+| ------------- | ------------- |
+| file  | unknown.jpg |
+| classId  | 6049da1fc125133d04bdad6b |
+```
---
+++ app.py
@@ -0,0 +1,62 @@
+import flask
+from flask import request, jsonify
+import face_recognition
+import numpy as np
+import urllib
+import requests
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def api_id():
+    known_image_encodings = []
+    known_face_ids = []
+    try:
+        token = request.headers['Authorization']
+    except KeyError:
+        return jsonify({"message": "Not authorized, no token"}), 401
+    class_id = request.form['classId']
+    req_url = f'https://attendancesystemadmin.herokuapp.com/api/student/byClass/{class_id}'
+    headers = {"Authorization": f'{token}'}
+
+    response = requests.get(req_url, headers=headers)
+
+    if(response.status_code == 401):
+        return jsonify(response.json()), 401
+
+    students = response.json()
+
+    for student in students['students']:
+        known_face_ids.append(student['_id'])
+        response = urllib.request.urlopen(student['images'][0])
+        image = face_recognition.load_image_file(response)
+        encoding = face_recognition.face_encodings(image)
+        known_image_encodings.append(encoding[0])
+
+    file = request.files['file']
+    unknown_image = face_recognition.load_image_file(file)
+    face_locations = face_recognition.face_locations(
+        unknown_image, model="hog")
+    face_encodings = face_recognition.face_encodings(
+        unknown_image, face_locations)
+
+    face_ids = []
+
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(
+            known_image_encodings, face_encoding)
+        face_distances = face_recognition.face_distance(
+            known_image_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            known_id = known_face_ids[best_match_index]
+            face_ids.append(known_id)
+
+    results = {}
+    results['data'] = face_ids
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=80)
